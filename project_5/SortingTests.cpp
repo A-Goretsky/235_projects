839b5873e46784adb6a7a7d4266d5e583db2891f
Anton Goretsky <anton.goretsky@gmail.com>
2018-11-27 11:23:28 -0500

    add project_5 files

---
+++ project_5/SortingTests.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include "SortingTests.hpp"
+
+void selectionSort(int a[], size_t size) {
+	
+}
+
+void insertionSort(int a[], size_t size) {
+	
+}
+
+void mergeSort(int a[], int from, int to) {
+	
+}
+
+void generateRandomArray(int values[], size_t size) {
+	
+}
+
+void generateAscendingArray(int values[], size_t size) {
+	
+}
+
+void generateDescendingArray(int values[], size_t size) {
+	
+}
+
+void generateLastTenRandomArray(int values[], size_t size) {
+
+}
+
+
+double sortTest(void (*sortingFunction)(int a[], size_t size), int values[], size_t size) {
+
+}
+
+double mergeSortTest(int values[], size_t size) {
+
+}
+
+
---
+++ project_5/main.cpp
@@ -0,0 +1,5 @@
+#include "SortingTests.hpp"
+
+void selectionSort(int a[], size_t size) {
+
+}
